0348d1c48bcd9de29bf19cbdcef12d84fc0b828b
Yousef Mohamed <112489749+Yousefuwk20@users.noreply.github.com>
2024-04-06 20:34:11 +0200

    [1249] [Time Beats: 74.21%] [Memory Beats: 92.59%] - LeetPush

---
+++ 1249-Minimum-Remove-to-Make-Valid-Parentheses.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    std::string minRemoveToMakeValid(string s) {
+        int open = 0, close = 0, flag = 0;
+        for (char c : s) {
+            if (c == '(') {
+                open++;
+                flag++;
+            } else if (c == ')' && flag > 0) {
+                close++;
+                flag--;
+            }
+        }
+        
+        int k = min(open, close);
+        string ans = "";
+        open = k;
+        close = k;
+        for (char c : s) {
+            if (c == '(') {
+                if (open > 0) {
+                    ans += '(';
+                    open--;
+                }
+                continue;
+            }
+            if (c == ')') {
+                if (close > 0 && close > open) {
+                    ans += ')';
+                    close--;
+                }
+                continue;
+            } else {
+                ans += c;
+            }
+        }
+        return ans;
+    }
+};
